326869b36f5a4fdcc5955dbb04e61210270cb12c
William Stiner <wstiner@binghamton.edu>
2024-09-04 17:22:28 -0400

    class exercise

---
+++ ch01/exercises/data.py
@@ -0,0 +1,10 @@
+print(10 * 5) # Expected Answer:50
+print (10 ** 2) # Expected Answer:100
+print (15 / 10) # Expected Answer:1.5
+print (15 // 10) # Expected Answer:1
+print (-15 // 10) # Expected Answer:-1
+print (15 % 10) # Expected Answer:1
+print(10 % 15) # Expected Answer:0
+print (10 % 10) # Expected Answer:0
+print (0 % 10) # Expected Answer:0
+print (10 / 15) # Expected Answer:.66   the answer that was given is wrong because technically the .66s would repeat for infinity
