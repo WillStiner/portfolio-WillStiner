10d1b8df1cbe0f6833687fdbbe4975f6ed1be7b9
William Stiner <wstiner@binghamton.edu>
2024-09-09 17:06:28 -0400

    completed currency activity

---
+++ ch01/exercises/currency.py
@@ -0,0 +1,5 @@
+rate = int(input("What is the current exchange rate from Euros to Dollars?"))
+amount = int(input("How much currency are you exchanging?"))
+total = rate * amount
+result = total - 3
+print(result)
